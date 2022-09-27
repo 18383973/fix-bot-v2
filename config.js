cfd90ae82f7b002838b1052d575b4a94e1f33ec9
VincenttXD <113456734+18383973@users.noreply.github.com>
2022-09-28 05:18:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -119,11 +119,11 @@ global.author = '                「 VinnXD-MD  あ」'
 
 
 /*============== LOGO ==============*/
-global.thumb = 'https://i.ibb.co/tMcqcJS/uchiha-obito.jpg' //Main Thumbnail
-global.imagebot = 'https://i.ibb.co/tMcqcJS/uchiha-obito.jpg'
+global.thumb = 'https://i.ibb.co/SKy4Dyj/IMG-20220928-WA0006.jpg' //Main Thumbnail
+global.imagebot = 'https://i.ibb.co/SKy4Dyj/IMG-20220928-WA0006.jpg'
 global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
-global.thumbs = ['https://i.ibb.co/tMcqcJS/uchiha-obito.jpg']
-global.fla = 'https://i.ibb.co/tMcqcJS/uchiha-obito.jpg'
+global.thumbs = ['https://i.ibb.co/SKy4Dyj/IMG-20220928-WA0006.jpg']
+global.fla = 'https://i.ibb.co/SKy4Dyj/IMG-20220928-WA0006.jpg'
 global.flaaa2 = [
 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
